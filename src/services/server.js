8d4a57c7c7392a824101b593792bd10e36eba425
Magnitinka <magnitinka@yandex.ru>
2026-07-20 01:14:57 +0300

    Update server.js

--- src/services/server.js
+++ src/services/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
+console.log('Запуск сервера, порт:', PORT);
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
